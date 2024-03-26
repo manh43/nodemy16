6cd56a3d6d80a08f6fe48a41bddca69b2b5a9eca
lemanh <letienmanh1@gmail.com>
2024-03-26 14:23:36 +0700

    first commit

---
+++ bai1/bai1.js
@@ -0,0 +1,33 @@
+//tinh tong so le 1->100
+
+
+
+//tìm bcnn 3 9
+// for( var i=1;true;i++){
+//     if(i%3==0 && i%9==0){
+     
+//        break;
+//     } 
+// }
+// console.log(i);
+
+function check(value){
+
+
+    // i 2=>value
+    // value % i  return false
+    
+true
+    return 
+}
+
+var number =0;
+for (var i=1;i<100;i++){
+
+    if(check(i)){
+        number =number+i;
+        i++;
+    }
+  
+}
+console.log(number);
