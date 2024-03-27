558f5f5f97e1e3ffa3bafdc6acada259dfc8fcdb
lemanh <letienmanh1@gmail.com>
2024-03-27 11:25:19 +0700

    bai1

---
+++ bai1/thu.js
@@ -0,0 +1,28 @@
+// a=2;b=3
+// function tinhtong(a,b){
+//     sum=a+b;
+//     return sum;
+// }
+// tong=tinhtong(a,b);
+// console.log(tong);
+
+// let fruit="";
+// switch(fruit){
+//     case "banana":
+//         console.log("This is banana");
+//         break;
+//     case "apple":
+//         console.log("This is apple");
+//         break;
+//     case "lemon":
+//         console.log("This is lemon");
+//         break;
+//     default:
+//         console.log("unknow fruit");
+//         break;
+
+// }
+arr=[1,2,3,4];
+for(let i=0;i<arr.length;i++){
+console.log(arr[i]);
+}
